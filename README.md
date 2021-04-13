d8d039ac5de9dd2fed8b39ea0092ef89b3cd22d0
mik-p <mppritchard3@hotmail.com>
2021-04-13 06:49:34 +0000

    add default browser view with file-like navigation. still WIP

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 S3 object browser plugin with htm components for october-cms
 
 ## todo
-- add more s3 settings to backend form
-- add bucket and prefix variables to component
 - add upload component
 - add groups to test access control ideas
 - 
--- components/s3browser.php
+++ components/s3browser.php
@@ -15,6 +15,8 @@ class S3Browser extends ComponentBase
     
     public $url = '';
     
+    public $region = 'us-east-1';
+    
     public $access = '';
     
     public $secret = '';
@@ -32,13 +34,30 @@ class S3Browser extends ComponentBase
     public function defineProperties()
     {
         return [
-            'subPath' => [
-                 'title'             => 'Sub Path',
-                 'description'       => 'The optional S3 bucket sub-path',
-                 'default'           => '/',
+            'baseuri' => [
+                 'title'             => 'base uri',
+                 'description'       => 'the browser component base uri',
                  'type'              => 'string',
-                 'validationPattern' => '^[0-9]+$',
-                 'validationMessage' => 'The Sub Path property can contain only a uri'
+                 'required'          => true,
+                 'default'           => '/',
+                 'validationPattern' => '',
+                 'validationMessage' => 'the base uri property can contain only a uri'
+            ],
+            'bucket' => [
+                 'title'             => 'bucket',
+                 'description'       => 'the s3 bucket',
+                 'type'              => 'string',
+                 'required'          => true,
+                 'validationPattern' => '',
+                 'validationMessage' => 'the bucket property can contain only a valid bucket name'
+            ],
+            'prefix' => [
+                 'title'             => 'path prefix',
+                 'description'       => 'the optional S3 bucket path prefix',
+                 'default'           => '',
+                 'type'              => 'string',
+                 'validationPattern' => '',
+                 'validationMessage' => 'the prefix property can contain only a uri'
             ]
         ];
     }
@@ -48,13 +67,14 @@ class S3Browser extends ComponentBase
         // get settings
         $this->activated = Settings::get('s3activated', false);
         $this->url = Settings::get('s3url', 'no-url');
+        $this->region = Settings::get('s3region', 'us-east-1');
         $this->access = Settings::get('s3accesskey', 'no-access');
         $this->secret = Settings::get('s3secretkey', 'no-secret');
         
         // connect to s3 with given credentials
         $this->storage_client = new S3Client([
             'version' => 'latest',
-            'region'  => 'us-east-1',
+            'region'  => $this->region,
             'endpoint' => $this->url,
             'use_path_style_endpoint' => true,
             'credentials' => [
@@ -62,10 +82,116 @@ class S3Browser extends ComponentBase
                     'secret' => $this->secret,
                 ],
         ]);
+    }
+    
+    public function listBuckets()
+    {
+        $bucketListResponse = $this->storage_client->listBuckets();
+        return $bucketListResponse['Buckets'];
+    }
+    
+    public function getObjects()
+    {
+        $current_prefix = '';
         
-        $buckets = $this->storage_client->listBuckets();
-        foreach ($buckets['Buckets'] as $bucket) {
-             $this->subPaths[] = $bucket['Name'];
+        if (is_string($this->property('prefix')))
+        {
+            $current_prefix = $this->property('prefix');
         }
+        
+        $objectsListResponse = $this->storage_client->listObjects([
+            'Bucket' => $this->property('bucket'),
+            'Prefix' => $current_prefix
+        ]);
+        
+        $objects = [];
+        
+        foreach ($objectsListResponse['Contents'] as $object) {
+            $unprefixed_key = ltrim($object['Key'], $current_prefix.'/');
+            $exploded_key = explode('/', $unprefixed_key);
+            
+            if (count($exploded_key) == 1)
+            {
+                $object['ShortName'] = $exploded_key[0];
+                $objects[] = $object;
+            }
+        }
+
+        return $objects;
+    }
+    
+    public function getPrefixes()
+    {
+        $current_prefix = '';
+        
+        if (is_string($this->property('prefix')))
+        {
+            $current_prefix = $this->property('prefix');
+        }
+        
+        $objectsListResponse = $this->storage_client->listObjects([
+            'Bucket' => $this->property('bucket'),
+            'Prefix' => $current_prefix
+            //'Delimiter' => '/'
+        ]);
+        
+        $crumbs = $this->getBreadCrumbs();
+        
+        $prefixes = [];
+        
+        foreach ($objectsListResponse['Contents'] as $object) {
+            $unprefixed_key = $object['Key'];
+            
+            foreach ($crumbs as $crumb)
+            {
+                $unprefixed_key = ltrim($object['Key'], $current_prefix.'/');
+            }
+            
+            $exploded_key = explode('/', $unprefixed_key);
+            
+            echo print_r($exploded_key);
+            
+            if (count($exploded_key) == 2)
+            {
+                $prefixes[] = $exploded_key[0];
+            }
+        }
+        
+        return array_unique($prefixes);
+    }
+    
+    public function getBreadCrumbs()
+    {
+        $current_prefix = '';
+        
+        if (is_string($this->property('prefix')))
+        {
+            $current_prefix = $this->property('prefix');
+        }
+        
+        $crumbs = explode('/', $current_prefix);
+        
+        return $crumbs;
+    }
+    
+    public function onDownload()
+    {
+        # lets download our file 
+        $file_to_download = 'from_php_script/lullaby.txt'; # private file: 'from_php_script/lullaby-private.txt' 
+        $download_as_path = __dir__.'/downloads/lullaby.txt'; 
+        
+        # save object to a file 
+        $result = $s3->getObject([ 
+        	'Bucket' => $config['s3-access']['bucket'], 
+        	'Key' => $file_to_download, 
+        	'SaveAs' => $download_as_path 
+        ]);
+    }
+    
+    public function doTests()
+    {
+        $result = '';
+        
+        echo print_r($result);
     }
 }
